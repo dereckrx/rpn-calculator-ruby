5f483acf5aca5ff74d252313bf02179c96aa6a3e
Dereck Rasmussen <dereckrx@gmail.com>
2013-07-13 17:20:44 -0700

    Merged rpn_stack into rpn_calculator.

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -1,20 +1,35 @@
 =begin 
 
-This class iterates through a tokenized calculator expression and:
+This class iterates through a tokenized expression and:
 
  * if it's a value, pushes it on the stack
  * if it's an operator, evalutates it and pushes its result on the stack
  * else raises an invalid token error
+ 
+Single values can be pushed on the stack, but only value pairs can be popped. 
+An RPN expression is valid only if one value remains on the stack after all 
+the operators have been evaluated. 
+
+The calculator will raise errors if the number of 
+remaining values is incorrect for #pop_pair or #pop_result
 
 So if the input tokens are ['1', '2', '+', '4', '*']:
 
- * 1 would push 1 onto the stack.
- * 2 would then push 2 onto the stack (so the stack is now [1,2] with 3 on top)
- * + would remove the top two stack entries from the stack (1 and 2),
-   add them together and put the result on the stack. So the stack would now be [3].
- * 4 would push 4 onto the stack [3,4]
- * '*' would pop the top two entries and mulitply them together, and push 12 onto the stack.
- * When no tokens are left, the last value on the stack [12] is returned as the result.
+ * Push 1 onto the stack.
+ 
+ * Push 2 onto the stack. So the stack is now [1,2] with 3 on top)
+ 
+ * '+' pop the top two stack entries from the stack (1 and 2), add them 
+   together and put the result (3) on the stack. So the stack would now be 
+   [3].
+   
+ * Push 4 onto the stack. So the stack  is now [3,4].
+ 
+ * '*' pop the top two entries and mulitply them together, and push 12 
+   onto the stack. The stack is now [12].
+ 
+ * When no tokens are left, the last value on the stack (12) is returned as 
+   the result.
 
 == Example
   
@@ -24,32 +39,35 @@ So if the input tokens are ['1', '2', '+', '4', '*']:
 =end 
 
 require_relative 'operation_bank'
-require_relative 'rpn_stack'
 require_relative 'rpn_error'
 
 class RpnCalculator
 
   # Returns the number that results from evaluating the tokenized epression.
   def evaluate(tokens)
-    stack.reset
+    reset
     
     tokens.each do |token|
       if is_operator?(token)
-        stack.push(evaluate_operator(token))
+        push(evaluate_operator(token))
       elsif is_value?(token)
-        stack.push(token.to_f)
+        push(token.to_f)
       else
         raise RpnError, "Invalid token '#{token}'." 
       end
     end
     
-    stack.pop_result
+    pop_result
   end
   
   private
   
-  def stack
-    @stack ||= RpnStack.new
+  def reset
+    @stack = []
+  end
+  
+  def push(value)
+    @stack.push(value)
   end
   
   def operation_bank
@@ -65,8 +83,18 @@ class RpnCalculator
   end
   
   def evaluate_operator(operator)
-    y, x = stack.pop_pair
+    y, x = pop_pair
     operation_bank.call(operator, x, y)
   end
-  
+
+  def pop_pair
+    raise RpnError, "Not enough values." if @stack.length < 2
+    [@stack.pop, @stack.pop]
+  end
+
+  def pop_result
+    raise RpnError, "Not enough operators." if @stack.length > 1
+    @stack.pop
+  end
+
 end
